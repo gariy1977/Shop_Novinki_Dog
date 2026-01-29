221d59d13a9349cdf45be3cc2b39ccd7924da6fd
gariy1977 <100241988+gariy1977@users.noreply.github.com>
2026-01-29 07:50:28 +0300

    Update filters.py

--- core/filters.py
+++ core/filters.py
@@ -1,1 +1,14 @@
 
+from core.product import Product
+
+
+def filter_by_brand(product: Product, allowed_brands: list) -> bool:
+    return any(b.lower() in product.name.lower() for b in allowed_brands)
+
+
+def filter_by_price(product: Product, min_price: int, max_price: int) -> bool:
+    try:
+        price = int(''.join(filter(str.isdigit, product.price)))
+        return min_price <= price <= max_price
+    except:
+        return False
