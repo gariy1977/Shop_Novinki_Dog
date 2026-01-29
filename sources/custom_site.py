d8aa5d04e98c99fb4cbd1c6851f1b7bda7cab867
gariy1977 <100241988+gariy1977@users.noreply.github.com>
2026-01-29 08:17:45 +0300

    Update custom_site.py

--- sources/custom_site.py
+++ sources/custom_site.py
@@ -1,1 +1,11 @@
 
+from sources.base import BaseSource
+from core.product import Product
+
+
+class CustomSiteSource(BaseSource):
+    name = "custom_site"
+
+    async def search(self, query: str):
+        # TODO: реализовать парсер сайта / API
+        return []
